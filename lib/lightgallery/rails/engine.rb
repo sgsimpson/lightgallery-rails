b2936fc7ac2e94a592247a75c158fc889e6de15b
bsboris <bsboris@gmail.com>
2017-06-09 13:21:42 +0300

    Precompile fonts

--- lib/lightgallery/rails/engine.rb
+++ lib/lightgallery/rails/engine.rb
@@ -1,6 +1,9 @@
 module Lightgallery
   module Rails
     class Engine < ::Rails::Engine
+      initializer "lightgallery-rails.assets.precompile" do |app|
+        app.config.assets.precompile << %r{lightgallery/lg\.(?:eot|svg|ttf|woff)$}
+      end
     end
   end
 end
