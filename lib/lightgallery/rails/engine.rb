156e6ebdd6efbeaacc33b05424629f085ab38daa
Scott Simpson <scott@simpson.kiwi>
2019-10-11 19:30:18 +1300

    Update engine.rb

--- lib/lightgallery/rails/engine.rb
+++ lib/lightgallery/rails/engine.rb
@@ -2,7 +2,9 @@ module Lightgallery
   module Rails
     class Engine < ::Rails::Engine
       initializer "lightgallery-rails.assets.precompile" do |app|
-        app.config.assets.precompile << %r{lightgallery/lg\.(?:eot|svg|ttf|woff)$}
+        unless Sprockets::Rails::VERSION.starts_with?('3')
+          app.config.assets.precompile << %r{lightgallery/lg\.(?:eot|svg|ttf|woff)$}
+        end
       end
     end
   end
